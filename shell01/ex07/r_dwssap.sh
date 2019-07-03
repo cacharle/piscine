4205989a932533f528c0fa8f76126a5b57606c0a
Charles <sircharlesaze@gmail.com>
2019-07-03 10:38:29 +0200

    shell01 ex04 corr

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/ *#.*/d' | awk 'NR % 2 == 0 { print }' | cut -d : -f 1 | rev | sort -r | awk 'NR >= '$FT_LINE1' && NR <= '$FT_LINE2' { print }' | paste -sd "," - | sed "s/\,/, /g" | sed "s/$/./"
+cat /etc/passwd | sed '/ *#.*/d' | awk 'NR % 2 == 0 { print }' | cut -d : -f 1 | rev | sort -r | awk 'NR >= '$FT_LINE1' && NR <= '$FT_LINE2' { print }' | paste -sd "," - | sed "s/\,/, /g" | sed "s/$/./" | tr -d "\n"
