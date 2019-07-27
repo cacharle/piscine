f365ae69ad3923b5c565293d34c636d20991bd56
Charles <sircharlesaze@gmail.com>
2019-07-27 08:35:04 +0200

    adding bsq generator, remove rush01 6x6 try

---
+++ bsq/gen.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+my ($x, $y, $density) = @ARGV;
+
+print "$y.ox\n";
+for (my $i = 0; $i < $y; $i++) {
+    for (my $j = 0; $j < $x; $j++) {
+        if (int(rand($y) * 2) < $density) {
+            print "o";
+        }
+        else {
+            print ".";
+        }
+    }
+    print "\n";
+}
+
+
